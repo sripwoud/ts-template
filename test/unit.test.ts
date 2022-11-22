350f8a85e6c55e695131b0de067bd329e6362c6e
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-22 11:51:38 +0100

    Format

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -1,7 +1,7 @@
-import {TEST} from "@mod/file";
+import { TEST } from '@mod/file'
 
 describe('Test', () => {
-    it('some test', () => {
-        expect(TEST).toEqual('abc')
-    })
+  it('some test', () => {
+    expect(TEST).toEqual('abc')
+  })
 })
