945bb28ec4869bcfa78eb949fb11796c6db085c9
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-06 17:04:24 +0100

    update test.ci script

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -1,22 +1,6 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
 
-import common from './jest.common'
-
 const jestConfig: JestConfigWithTsJest = {
-  ...common,
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**'],
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
-  },
   projects: [
     './test/jest.lint.ts',
     './test/jest.prettier.ts',
--- test/jest.unit.ts
+++ test/jest.unit.ts
@@ -4,6 +4,19 @@ import common from './jest.common'
 
 const jestUnitConfig: JestConfigWithTsJest = {
   ...common,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
   displayName: 'unit',
   setupFilesAfterEnv: ['./test/setup.ts'],
 }
