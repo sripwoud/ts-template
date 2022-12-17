8e6e4070fcebb63111f410307d926d8462705715
3pwd <3pwd@tuta.io>
2022-12-17 15:09:03 +0100

    :test_tube: Add prettier and lint jest runners (#21)
    - config jest test runner
    - add watch select projects plugin
    - add watch typeahead plugin
    - add jest prettier runner

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -1,31 +1,19 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
-import { pathsToModuleNameMapper } from 'ts-jest'
-
-import { compilerOptions } from '../tsconfig.json'
 
 const jestConfig: JestConfigWithTsJest = {
-  clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**'],
+  collectCoverageFrom: ['<rootDir>/src/**'],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['src/index.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
-  },
-  moduleDirectories: ['node_modules', __dirname],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
-  preset: 'ts-jest',
-  rootDir: '..',
-  setupFilesAfterEnv: ['./test/setup.ts'],
-  verbose: true,
+  projects: [
+    './test/jest.lint.ts',
+    './test/jest.prettier.ts',
+    './test/jest.unit.ts',
+  ],
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
 
 export default jestConfig
