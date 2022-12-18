0fd05a1f69eb6bc26d70fcf7af8301b1f16ca7b8
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-18 11:02:33 +0100

    Update jest config files

--- test/jest.lint.ts
+++ test/jest.lint.ts
@@ -6,7 +6,7 @@ const jestLintConfig: JestConfigWithTsJest = {
   ...common,
   displayName: 'lint',
   runner: 'jest-runner-eslint',
-  testMatch: ['<rootDir>/**/*.(cjs|mjs|jsx|ts|tsx)'],
+  testMatch: ['<rootDir>/**/*.(cjs|mjs|js|jsx|ts|tsx)'],
   testPathIgnorePatterns: ['<rootDir>/test/coverage'],
 }
 
