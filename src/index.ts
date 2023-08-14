14905b1fd8eeae753c28f56fade68966625a72ba
sripwoud <me@sripwoud.xyz>
2023-08-14 12:24:41 +0200

    use personal tsconfig package

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ interface B {
 const c: B = { foo: 'bar' }
 
 console.log(A, c, TEST, 1)
-console.log(process.env.SECRET)
-console.log(process.env.FOO)
+console.log(process.env['SECRET'])
+console.log(process.env['FOO'])
--- test/jest.unit.ts
+++ test/jest.unit.ts
@@ -1,29 +1,36 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
-
-import { pathsToModuleNameMapper } from 'ts-jest'
-import { compilerOptions } from '../tsconfig.json'
+import type {JestConfigWithTsJest} from 'ts-jest'
+import {pathsToModuleNameMapper} from 'ts-jest'
+import assert from 'assert'
+import {compilerOptions} from '../tsconfig.json'
 import common from './jest.common'
 
-const jestUnitConfig: JestConfigWithTsJest = {
-  ...common,
-  clearMocks: true,
-  coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
-  },
-  displayName: 'unit',
-  moduleDirectories: ['node_modules', __dirname],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
-  }),
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['jest-chain', './test/setup.ts'],
+})
+
+assert(moduleNameMapper !== undefined)
+
+const jestUnitConfig: JestConfigWithTsJest = {
+    ...common,
+    clearMocks: true,
+    coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70,
+        },
+    },
+    displayName: 'unit',
+    moduleDirectories: ['node_modules', __dirname],
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/',
+    })!,
+    preset: 'ts-jest',
+    setupFilesAfterEnv: ['jest-chain', './test/setup.ts'],
 }
 
 export default jestUnitConfig
