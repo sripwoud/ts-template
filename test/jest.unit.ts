2d83c642cb9cc73522d83a6990a5a1b92a7cf35e
sripwoud <me@sripwoud.xyz>
2023-08-14 12:36:11 +0200

    format

--- test/jest.unit.ts
+++ test/jest.unit.ts
@@ -1,36 +1,36 @@
-import type {JestConfigWithTsJest} from 'ts-jest'
-import {pathsToModuleNameMapper} from 'ts-jest'
+import type { JestConfigWithTsJest } from 'ts-jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
 import assert from 'assert'
-import {compilerOptions} from '../tsconfig.json'
+import { compilerOptions } from '../tsconfig.json'
 import common from './jest.common'
 
 const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
+  prefix: '<rootDir>/',
 })
 
 assert(moduleNameMapper !== undefined)
 
 const jestUnitConfig: JestConfigWithTsJest = {
-    ...common,
-    clearMocks: true,
-    coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
-    coverageThreshold: {
-        global: {
-            branches: 70,
-            functions: 70,
-            lines: 70,
-            statements: 70,
-        },
+  ...common,
+  clearMocks: true,
+  coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
-    displayName: 'unit',
-    moduleDirectories: ['node_modules', __dirname],
-    moduleFileExtensions: ['ts', 'js', 'json'],
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: '<rootDir>/',
-    })!,
-    preset: 'ts-jest',
-    setupFilesAfterEnv: ['jest-chain', './test/setup.ts'],
+  },
+  displayName: 'unit',
+  moduleDirectories: ['node_modules', __dirname],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  })!,
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['jest-chain', './test/setup.ts'],
 }
 
 export default jestUnitConfig
