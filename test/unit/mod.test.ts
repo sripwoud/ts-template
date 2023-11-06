274eb539abc54f53c4f6e9d3e7168e797cac5f6f
sripwoud <me@sripwoud.xyz>
2023-11-06 11:54:05 +0100

    lint, format

--- test/unit/mod.test.ts
+++ test/unit/mod.test.ts
@@ -1,8 +1,8 @@
-import { TEST } from '@mod'
 import { describe, expect, it } from 'bun:test'
+import { TEST } from '@mod'
 
 describe('Test', () => {
-  it('some test', () => {
-    expect(TEST).toEqual('abc')
-  })
+	it('some test', () => {
+		expect(TEST).toEqual('abc')
+	})
 })
